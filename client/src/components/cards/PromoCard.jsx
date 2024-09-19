712e8b840920fa1806cd3dd69d1163d4630c22c7
Kaustubh <132165310+kaustubh-aras@users.noreply.github.com>
2024-09-19 13:48:05 +0530

    Modified PromoCard and Made it responsive

--- client/src/components/cards/PromoCard.jsx
+++ client/src/components/cards/PromoCard.jsx
@@ -1,36 +1,49 @@
 import React from "react";
+import Container from "../utils/Container";
 
-const PromoCard = () => {
+const PromoCard = ({
+  bgColor = "#ECF494",
+  title = "Subscribe",
+  discount = "10% OFF",
+  subtitle = "for first order",
+  buttonText = "SUBSCRIBE",
+  buttonColor = "bg-black",
+  buttonHoverColor = "hover:bg-gray-800",
+  textColor = "text-[#333]",
+  fontSize = "text-3xl lg:text-4xl",
+}) => {
   return (
-    <div className="bg-[#ECF494] gap-">
-      <div className="py-12 flex flex-col lg:flex-row justify-center items-center space-y-6 lg:space-y-0 lg:space-x-6">
-        <div className="text-center lg:text-left lg:mr-32">
-          <span className="font-inter font-bold text-3xl lg:text-4xl">
-            Subscribe
-          </span>
-          <span className="font-inter font-normal text-3xl lg:text-4xl">
-            {" "} & Get{" "}
-          </span>
-          <span className="font-inter font-bold text-3xl lg:text-4xl">
-            10% OFF
-          </span>
-          <span className="font-inter font-normal text-3xl lg:text-4xl">
-            {" "} for first order
-          </span>
+    <div className={`bg-[${bgColor}]`}>
+      <Container>
+        <div className="py-12 flex flex-col lg:flex-row justify-center items-center space-y-6 lg:space-y-0 lg:space-x-6">
+          <div className={`text-center lg:text-left lg:mr-32 ${textColor}`}>
+            <span className={`font-inter font-bold ${fontSize}`}>{title}</span>
+            <span className={`font-inter font-normal ${fontSize}`}>
+              {" "}
+              & Get{" "}
+            </span>
+            <span className={`font-inter font-bold ${fontSize}`}>
+              {discount}
+            </span>
+            <span className={`font-inter font-normal ${fontSize}`}>
+              {" "}
+              {subtitle}
+            </span>
+          </div>
+          <div className="flex items-center max-w-md">
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="flex-grow p-4 rounded-l-full border border-gray-300 focus:outline-none"
+            />
+            <button
+              className={`p-4 ${buttonColor} text-white rounded-r-full ${buttonHoverColor} focus:outline-none`}
+            >
+              {buttonText}
+            </button>
+          </div>
         </div>
-        <div className="flex items-center w-full max-w-md">
-          <input
-            type="email"
-            placeholder="Enter your email address"
-            className="flex-grow p-4 rounded-l-full border border-gray-300 focus:outline-none"
-          />
-          <button
-            className="p-4 bg-black text-white rounded-r-full hover:bg-gray-800 focus:outline-none"
-          >
-            SUBSCRIBE
-          </button>
-        </div>
-      </div>
+      </Container>
     </div>
   );
 };
